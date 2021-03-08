68d9f562b66ccbfc7a1e82223221a28e5e52d5c1
Sadiq <samin.sada@gmail.com>
2021-03-07 21:51:13 -0500

    output_board_ps fixed header and footer strcat to strcpy

--- hw2/src/drivers.c
+++ hw2/src/drivers.c
@@ -477,7 +477,7 @@ game *g;
 
   /* header file */
   (void)strcpy(chaine, LIB_DIR);
-  if ((ftmp = fopen(strcat(chaine, PS_HEADER), "r")) == NULL)
+  if ((ftmp = fopen(strcpy(chaine, PS_HEADER), "r")) == NULL)
     message((stderr, "Can't open ps header file.\n"));
   else
   {
@@ -501,7 +501,7 @@ game *g;
 
   /* footer file */
   (void)strcpy(chaine, LIB_DIR);
-  if ((ftmp = fopen(strcat(chaine, PS_FOOTER), "r")) == NULL)
+  if ((ftmp = fopen(strcpy(chaine, PS_FOOTER), "r")) == NULL)
     message((stderr, "Can't open ps footer file.\n"));
   else
   {
