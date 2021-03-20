fdec58b53735cec7bfeaf06d38eb0aeb7cd1f93f
Md Sadiq Sada <msada@cs.stonybrook.edu>
2021-03-19 22:05:54 -0400

    prologue working

--- hw3/src/main.c
+++ hw3/src/main.c
@@ -1,14 +1,33 @@
 #include <stdio.h>
 #include "sfmm.h"
 
-int main(int argc, char const *argv[]) {
-    double* ptr = sf_malloc(sizeof(double));
+#define WSIZE 2
+#define DSIZE 4
 
-    *ptr = 320320320e-320;
+#define PUT(p, val) (*(unsigned int *)(p) = (val))
+#define PACK(size, al, pal) ((size) | (al) | (pal << 1))
 
-    printf("%f\n", *ptr);
+int main(int argc, char const *argv[])
+{
+    sf_mem_grow();
 
-    sf_free(ptr);
+    for (int i = 0; i < NUM_FREE_LISTS; i++)
+    {
+        sf_free_list_heads[i].body.links.next = &sf_free_list_heads[i];
+        sf_free_list_heads[i].body.links.prev = &sf_free_list_heads[i];
+    }
+
+    PUT(sf_mem_start() + 8, PACK(32, 1, 0));
+    PUT(sf_mem_end() - 8, PACK(0, 1, 0));
+
+    sf_show_heap();
+    // double *ptr = malloc(sizeof(double));
+
+    // *ptr = 320320320e-320;
+
+    // printf("%f\n", *ptr);
+
+    // free(ptr);
 
     return EXIT_SUCCESS;
 }
--- hw3/src/sfmm.c
+++ hw3/src/sfmm.c
@@ -8,18 +8,70 @@
 #include "debug.h"
 #include "sfmm.h"
 
-void *sf_malloc(size_t size) {
+#define WSIZE 2
+#define DSIZE 4
+
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
+
+#define PACK(size, alloc) ((size) | (alloc))
+
+#define GET(p) (*(unsigned int *))(p))
+#define PUT(p, val) (*(unsigned int *)(p)) = (val))
+
+#define GET_SIZE(p) (GET(p) & ~0x7)
+#define GET_ALLOC(p) (GET(p) & 0x1)
+
+#define HDRP(bp) ((char *)(bp)-WSIZE)
+#define FTRP(bp) ((char *)(bp) + GET_SIZE(HDRP(bp)) - DSIZE
+
+#define NEXT_BLKP(bp) ((char *)(bp) + GET_SIZE(((char *)(bp)-WSIZE)))
+#define PREV_BLKP(bp) ((char *)(bp)-GET_SIZE(((char *)(bp)-DSIZE)))
+
+// finds next multiple of 16 for num input
+int findMultiple(int num)
+{
+    return (num % 16 != 0) ? num + (16 - (num % 16)) : num;
+}
+
+void *sf_malloc(size_t size)
+{
+    // if size is 0, return null
+    if (size == 0)
+        return NULL;
+
+    // determine padded size
+
+    int newSize = 0;
+
+    if (size <= 24)
+    {
+        // allocate block of size 32
+        newSize = 32;
+    }
+
+    else
+    {
+        // allocate block of size x + 8 + (padding)
+        newSize = size + 8;
+        newSize = findMultiple(newSize);
+    }
+
+    // search free list
+
     return NULL;
 }
 
-void sf_free(void *pp) {
+void sf_free(void *pp)
+{
     return;
 }
 
-void *sf_realloc(void *pp, size_t rsize) {
+void *sf_realloc(void *pp, size_t rsize)
+{
     return NULL;
 }
 
-void *sf_memalign(size_t size, size_t align) {
+void *sf_memalign(size_t size, size_t align)
+{
     return NULL;
 }
