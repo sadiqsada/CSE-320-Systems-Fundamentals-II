ebf9de553dc8009af1d588599d7e7dcac3549ffa
Md Sadiq Sada <msada@cs.stonybrook.edu>
2021-03-20 21:05:08 -0400

    add wilderness block

--- hw3/src/main.c
+++ hw3/src/main.c
@@ -9,25 +9,41 @@
 
 int main(int argc, char const *argv[])
 {
+    // get page of memory
     sf_mem_grow();
 
+    // initialize free lists
     for (int i = 0; i < NUM_FREE_LISTS; i++)
     {
         sf_free_list_heads[i].body.links.next = &sf_free_list_heads[i];
         sf_free_list_heads[i].body.links.prev = &sf_free_list_heads[i];
     }
 
-    PUT(sf_mem_start() + 8, PACK(32, 1, 0));
-    PUT(sf_mem_end() - 8, PACK(0, 1, 0));
+    // initialize prologue and epilogue
+    sf_block *block = (struct sf_block *)(sf_mem_start() + 8);
+    block->header = 32 + THIS_BLOCK_ALLOCATED + PREV_BLOCK_ALLOCATED;
 
+    block = (struct sf_block *)(sf_mem_end() - 8);
+    block->header = 0 + THIS_BLOCK_ALLOCATED + PREV_BLOCK_ALLOCATED;
+
+    // put rest of the memory in wilderness block
+    block = (struct sf_block *)(sf_mem_start() + 40);
+    PUT(&(block->header), PACK(8144, 0, 0));
+    PUT(&(block->header) + 8136, PACK(8144, 0, 0));
+
+    sf_block *startingBlock = (struct sf_block *)(&sf_free_list_heads[7]);
+    startingBlock->body.links.next = block;
+    startingBlock->body.links.prev = block;
+    block->body.links.next = startingBlock;
+    block->body.links.prev = startingBlock;
     sf_show_heap();
-    // double *ptr = malloc(sizeof(double));
+    // double *ptr = sf_malloc(sizeof(double));
 
     // *ptr = 320320320e-320;
 
     // printf("%f\n", *ptr);
 
-    // free(ptr);
+    // sf_free(ptr);
 
     return EXIT_SUCCESS;
 }
