01da9db94dbc4f6f24722185b101792ce6ba994d
Md Sadiq Sada <msada@cs.stonybrook.edu>
2021-03-26 20:15:11 -0400

    fix run failure

--- hw3/src/main.c
+++ hw3/src/main.c
@@ -3,9 +3,5 @@
 
 int main(int argc, char const *argv[])
 {
-    char *ptrA = sf_malloc(0); // Want a 50 byte block that is 128 aligned
-    *(ptrA) = 'A';
-
-    sf_show_heap();
     return EXIT_SUCCESS;
 }
