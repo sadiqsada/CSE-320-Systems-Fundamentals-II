d4b2f728cbb3fae0efe8faeb0ddbe1276814d009
Md Sadiq Sada <msada@cs.stonybrook.edu>
2021-04-30 22:38:38 -0400

    proto receive segfaulting

--- hw5/src/protocol.c
+++ hw5/src/protocol.c
@@ -4,7 +4,7 @@
 int proto_send_packet(int fd, CHLA_PACKET_HEADER *hdr, void *payload)
 {
     // convert to network byte order
-    uint32_t newPayloadLength = ntonl(hdr->payload_length);
+    uint32_t newPayloadLength = ntohl(hdr->payload_length);
 
     ssize_t writtenHeaderBytes = rio_writen(fd, hdr, sizeof(CHLA_PACKET_HEADER));
 
@@ -25,6 +25,20 @@ int proto_send_packet(int fd, CHLA_PACKET_HEADER *hdr, void *payload)
 
 int proto_recv_packet(int fd, CHLA_PACKET_HEADER *hdr, void **payload)
 {
+    ssize_t readHeaderBytes = rio_readn(fd, hdr, sizeof(CHLA_PACKET_HEADER));
 
+    if (readHeaderBytes == -1)
+        return -1;
+
+    if (hdr->payload_length != 0)
+    {
+        uint32_t newPayloadLength = ntohl(hdr->payload_length);
+        ssize_t readPayloadBytes = rio_readn(fd, payload, newPayloadLength);
+
+        if (readPayloadBytes == -1)
+        {
+            return -1;
+        }
+    }
     return 0;
 }
